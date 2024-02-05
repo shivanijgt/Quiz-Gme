0beb31bfaf9fdd7fb6a37d44fcf8f45f313b2098
shivanijgt <152775293+shivanijgt@users.noreply.github.com>
2024-02-05 20:51:13 +0530

    Add files via upload

---
+++ Component/QuestionBank.js
@@ -0,0 +1,65 @@
+const qBank = [
+
+    {
+
+        id: 1,
+
+        question: "Which of the following is the correct name of React.js?",
+
+        options: ["React", "React.js", "ReactJS", "All the above"],
+
+        answer: "All the above",
+
+    },
+
+    {
+
+        id: 2,
+
+        question: "Which of the following acts as the input of a class-based component?",
+
+        options: ["Class", "Factory", "Render", "Props"],
+
+        answer: "Props",
+
+    },
+
+    {
+
+        id: 3,
+
+        question: "Which of the following keyword is used to create a class inheritance?",
+
+        options: ["Create", "Inherits", "Extends", "This"],
+
+        answer: "Extends",
+
+    },
+
+    {
+
+        id: 4,
+
+        question: "What are the two ways to handle data in React?",
+
+        options: ["State & Props", "Serives & Components", "State & Service", "State & Component"],
+
+        answer: "State & Props",
+
+    },
+
+    {
+
+        id: 5,
+
+        question: "Does React.js create a VIRTUAL DOM in the memory?",
+
+        options: ["TRUE", "FALSE"],
+
+        answer: "TRUE",
+
+    },
+]
+ 
+
+export default qBank;
